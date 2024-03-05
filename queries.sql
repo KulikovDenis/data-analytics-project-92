3e3fe8cc7ceb0609fba466d65f73c07b021fa25c
Denis Kulikov <138636841+KulikovDenis@users.noreply.github.com>
2024-03-05 08:55:53 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ select count(customer_id) as customers_count
 from customers 
 
 
-Данный запрос вывод десять лучших продавцов по суммарнной выручке за все время:
+Данный запрос выводит десять лучших продавцов по суммарнной выручке за все время:
 select concat(employees.first_name, ' ', employees.last_name) as name,
 count(sales.sale_date) AS operations,
 COALESCE(round(sum(sales.quantity*products.price), 0), 0) as income
@@ -14,3 +14,21 @@ left join products on products.product_id = sales.product_id
 group by employees.first_name, employees.last_name
 order by income desc limit 10
 
+Данный запрос выводит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам:
+with av as (
+	select concat(employees.first_name, ' ', employees.last_name) as name,
+	COALESCE(round(sum(sales.quantity*products.price)/count(sales.sale_date), 0), 0) as average_income
+	from employees
+	left join sales on employees.employee_id = sales.sales_person_id
+	left join products on products.product_id = sales.product_id 
+	group by employees.first_name, employees.last_name
+)
+select *
+from av
+where average_income < (select 
+	COALESCE(round(sum(sales.quantity*products.price)/count(sales.sale_date), 0), 0) as average
+	from employees
+	left join sales on employees.employee_id = sales.sales_person_id
+	left join products on products.product_id = sales.product_id
+)
+order by average_income
