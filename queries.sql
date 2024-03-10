c5161e990a821e1bdcdac9e664cd3413abf4c82e
Denis Kulikov <138636841+KulikovDenis@users.noreply.github.com>
2024-03-10 15:41:59 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,21 +1,21 @@
-/*Данный запрос считает общее количество покупателей из таблицы customers*/
+/*Общее количество покупателей из таблицы customers*/
 
 select count(customer_id) as customers_count
 from customers;
 
 
-/*Данный запрос выводит десять лучших продавцов по суммарнной выручке за все время*/
+/*Десять лучших продавцов по суммарнной выручке за все время*/
 select 
-	concat(employees.first_name, ' ', employees.last_name) as seller,
-	count(sales.sale_date) AS operations,
-	coalesce(floor(sum(sales.quantity*products.price)), 0) as income
+    concat(employees.first_name, ' ', employees.last_name) as seller,
+	count(sales.sale_date) as operations,
+	coalesce(floor(sum(sales.quantity * products.price)), 0) as income
 from employees
 left join sales on employees.employee_id = sales.sales_person_id
-left join products on products.product_id = sales.product_id 
+left join products on products.product_id = sales.product_id
 group by employees.first_name, employees.last_name
 order by income desc limit 10;
 
-/*Данный запрос выводит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам*/
+/*Продавцы, чья средняя выручка сделки меньше средней выручки сделки по всем продавцам*/
 with av as (
 	select concat(employees.first_name, ' ', employees.last_name) as seller,
 	floor(sum(sales.quantity*products.price)/count(sales.sale_date)) as average_income
