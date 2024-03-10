7f914b9f6681424388ce19f54502618e68e3fb35
Denis Kulikov <138636841+KulikovDenis@users.noreply.github.com>
2024-03-10 15:17:36 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -5,9 +5,9 @@ from customers;
 
 
 Данный запрос выводит десять лучших продавцов по суммарнной выручке за все время:
-select concat(employees.first_name, ' ', employees.last_name) as name,
+select concat(employees.first_name, ' ', employees.last_name) as seller,
 count(sales.sale_date) AS operations,
-COALESCE(round(sum(sales.quantity*products.price), 0), 0) as income
+COALESCE(FLOOR(sum(sales.quantity*products.price)), 0) as income
 from employees
 left join sales on employees.employee_id = sales.sales_person_id
 left join products on products.product_id = sales.product_id 
@@ -16,8 +16,8 @@ order by income desc limit 10;
 
 Данный запрос выводит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам:
 with av as (
-	select concat(employees.first_name, ' ', employees.last_name) as name,
-	COALESCE(round(sum(sales.quantity*products.price)/count(sales.sale_date), 0), 0) as average_income
+	select concat(employees.first_name, ' ', employees.last_name) as seller,
+	floor(sum(sales.quantity*products.price)/count(sales.sale_date)) as average_income
 	from employees
 	left join sales on employees.employee_id = sales.sales_person_id
 	left join products on products.product_id = sales.product_id 
@@ -26,7 +26,7 @@ with av as (
 select *
 from av
 where average_income < (select 
-	COALESCE(round(sum(sales.quantity*products.price)/count(sales.sale_date), 0), 0) as average
+	floor(sum(sales.quantity*products.price)/count(sales.sale_date)) as average
 	from employees
 	left join sales on employees.employee_id = sales.sales_person_id
 	left join products on products.product_id = sales.product_id
@@ -34,38 +34,17 @@ where average_income < (select
 order by average_income;
 
 Данный запрос выводит информацию о выручке по дням недели:
-with week as (select concat(employees.first_name, ' ', employees.last_name) as name,
-case EXTRACT(DOW FROM sales.sale_date)
-	WHEN 0 THEN 'sunday'
-	WHEN 1 THEN 'monday'
-	WHEN 2 THEN 'tuesday'
-	WHEN 3 THEN 'wednesday'
-	WHEN 4 THEN 'thursday'
-	WHEN 5 THEN 'friday'
-	WHEN 6 THEN 'saturday'
-end as weekday,
-coalesce(round(sum(sales.quantity*products.price), 0), 0) as income
-from employees
-left join sales on employees.employee_id = sales.sales_person_id
-left join products on products.product_id = sales.product_id 
-group by employees.first_name, employees.last_name, weekday
-)
-
-select name, weekday, income
-from week
-where income > '0'
-order by case weekday
-	WHEN 'monday' THEN 1
-	WHEN 'tuesday' THEN '2'
-	WHEN 'wednesday' THEN '3'
-	WHEN 'thursday' THEN '4'
-	WHEN 'friday' THEN '5'
-	WHEN 'saturday' THEN '6'
-	when 'sunday' THEN '7'
-end, name;
+with tab as(
+select * from (select concat(first_name,' ', last_name) as seller, to_char(sale_date, 'day') as day_of_week, to_char(sale_date, 'ID') as id,
+floor(sum(quantity*price)) as income from employees 
+join sales on employee_id=sales_person_id
+join products on sales.product_id=products.product_id
+group by 1, 2, 3) a
+order by 3, seller)
+select seller, day_of_week, income from tab;
 
 Данный запрос выводит количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+:
-select age_category, count(1) from(
+select age_category, count(1) as age_count  from(
 select first_name, last_name, age,
 case 
 when age between 16 and 25 then '16-25'
@@ -77,14 +56,14 @@ group by age_category order by 1;
 
 
 Данный запрос выводит данные по количеству уникальных покупателей и выручке, которую они принесли:
-SELECT TO_CHAR(sale_date , 'yyyy-MM') as date,
+SELECT TO_CHAR(sale_date , 'yyyy-MM') as selling_month,
 count(distinct  concat(first_name,' ', last_name))as total_customers,
-round(sum(quantity*price), 0) as income
+floor(sum(quantity*price)) as income
 FROM customers
 join sales on customers.customer_id = sales.customer_id
 join products on sales.product_id=products.product_id
-group by date
-order by date;
+group by selling_month
+order by selling_month;
 
 
 Данный запрос выводит данные о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0):
