115e16eb1edfd79477fb1c757cb0c22ef191b2c5
Denis Kulikov <138636841+KulikovDenis@users.noreply.github.com>
2024-03-12 09:09:43 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ select
     count(sales.sale_date) as operations,
     coalesce(floor(sum(sales.quantity * products.price)), 0) as income
 from employees
-left join sales on sales.sales_person_id = employees.employee_id
+left join sales on employees.employee_id = sales.sales_person_id
 left join products on products.product_id = sales.product_id
 group by employees.first_name, employees.last_name
 order by income desc limit 10;
@@ -18,22 +18,24 @@ order by income desc limit 10;
 /*Продавцы, чья ср. выручка сделки меньше ср. выручки сделки по всем продавцам*/
 with av as (
     select
-	    concat(employees.first_name, ' ', employees.last_name) as seller,
-	    floor(sum(sales.quantity * products.price) / count(sales.sale_date)) as average_income
-    from employees
-    left join sales on sales.sales_person_id = employees.employee_id
-    left join products on products.product_id = sales.product_id 
+        concat(employees.first_name, ' ', employees.last_name) as seller,
+        floor(sum(s.quantity * p.price) / count(s.sale_date)) as average_income
+    from employees as e
+    left join sales as s on e.employee_id = s.sales_person_id
+    left join products as p on p.product_id = s.product_id
     group by employees.first_name, employees.last_name
 )
 
-    select
-        *
-    from av
-    where average_income < (select 
-	    floor(sum(sales.quantity * products.price) / count(sales.sale_date)) as average
-	    from employees
-	    left join sales on employees.employee_id = sales.sales_person_id
-	    left join products on products.product_id = sales.product_id
+select
+    *
+from av
+where
+    average_income < (
+        select 
+            floor(sum(s.quantity * p.price) / count(s.sale_date)) as average
+        from employees as e
+        left join sales as s on e.employee_id = s.sales_person_id
+        left join products as p on p.product_id = s.product_id
     )
 order by average_income;
 
@@ -53,6 +55,7 @@ with tab as (
     )
     order by id, seller
 )
+
 select
     seller,
     day_of_week,
@@ -80,35 +83,35 @@ order by age_category;
 
 
 /*Количество уникальных покупателей и выручке, которую они принесли*/
-select 
-	to_char(sale_date , 'yyyy-MM') as selling_month,
-	count(distinct  concat(first_name,' ', last_name))as total_customers,
-	floor(sum(quantity*price)) as income
-from customers
-join sales on customers.customer_id = sales.customer_id
-join products on sales.product_id = products.product_id
+select
+    to_char(s.sale_date, 'yyyy-MM') as selling_month,
+    count(distinct concat(c.first_name, ' ', c.last_name)) as total_customers,
+    floor(sum(s.quantity * p.price)) as income
+from customers as c
+inner join sales as s on customers.customer_id = s.customer_id
+inner join products as p on s.product_id = p.product_id
 group by selling_month
 order by selling_month;
 
 
 /*Покупатели, первая покупка которых была в ходе проведения акций*/
-select 
-	customer,
-	sale_date,
-	seller 
+select
+    customer,
+    sale_date,
+    seller
 from (
-select 
-distinct on(c.customer_id) c.customer_id, 
-concat(c.first_name,' ', c.last_name) as customer, 
-sale_date, 
-sales_id, 
-concat(e.first_name,' ', e.last_name) as seller, 
-p.price 
-from sales s
-join customers c on c.customer_id = s.customer_id
-join products p on s.product_id = p.product_id
-join employees e on s.sales_person_id = e.employee_id
-group by 1, 2, 3, 4, 5, 6
-having price = 0
-order by 1, 3
-) a;
+    select 
+        distinct on (c.customer_id) c.customer_id, 
+        concat(c.first_name,' ', c.last_name) as customer, 
+        sale_date, 
+        sales_id, 
+        concat(e.first_name,' ', e.last_name) as seller, 
+        p.price 
+    from sales s
+    inner join customers c on c.customer_id = s.customer_id
+    inner join products p on s.product_id = p.product_id
+    inner join employees e on s.sales_person_id = e.employee_id
+    group by 1, 2, 3, 4, 5, 6
+    having price = 0
+    order by 1, 3
+);
